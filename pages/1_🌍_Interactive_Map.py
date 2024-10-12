e1c753f3fcf32a6b07db04cfd7cd3722905189cc
ship123talk456 <158138042+ship123talk456@users.noreply.github.com>
2024-10-12 16:06:25 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -26,7 +26,7 @@ with col2:
 with col1:
 
     m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+        locate_control=True, latlon_control=True, minimap_control=True
     )
     m.add_basemap(basemap)
     m.to_streamlit(height=700)
